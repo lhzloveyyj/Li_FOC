b65a1bf4bb815d935de639873ea369e1924732e9
lhz <2937739768@qq.com>
2025-11-26 21:51:33 +0800

    可以直接转，iqid不是正弦

--- AT32F403ACCT7_WorkBench/project/Hardware/FOC.c
+++ AT32F403ACCT7_WorkBench/project/Hardware/FOC.c
@@ -202,9 +202,9 @@ float g_pwmC = 0.0f;
 ******************************************************************************/
 static void setpwm_channel(float pwm_a, float pwm_b, float pwm_c)
 {
-	tmr_channel_value_set(TMR1, TMR_SELECT_CHANNEL_3, pwm_a * FOC_ALL_DUTY * 0.95f);
+	tmr_channel_value_set(TMR1, TMR_SELECT_CHANNEL_1, pwm_a * FOC_ALL_DUTY * 0.95f);
     tmr_channel_value_set(TMR1, TMR_SELECT_CHANNEL_2, pwm_b * FOC_ALL_DUTY * 0.95f);
-    tmr_channel_value_set(TMR1, TMR_SELECT_CHANNEL_1, pwm_c * FOC_ALL_DUTY * 0.95f);
+    tmr_channel_value_set(TMR1, TMR_SELECT_CHANNEL_3, pwm_c * FOC_ALL_DUTY * 0.95f);
 }
 
 /**
@@ -299,7 +299,7 @@ void FocContorl(PFocState pFOC,  PSVpwm_State PSVpwm)
     pFOC->Ic = (pFOC->current.adC - pFOC->current.voltageCOffset)/4096.0f * FOC_ADC_REF_VOLTAGE / FOC_GAIN / FOC_SHUNT_R;
 	
     // 因为ia采样有点问题，暂时先用ibic  
-	clarke_transform(pFOC->Ia, pFOC->Ib, &pFOC->iAlpha, &pFOC->iBeta);
+	clarke_transform(-pFOC->Ib, -pFOC->Ia, &pFOC->iAlpha, &pFOC->iBeta);
     park_transform(pFOC->iAlpha, pFOC->iBeta, pFOC->correctedAngle, &pFOC->id, &pFOC->iq);
     
 	//PID控制器
@@ -316,12 +316,12 @@ void FocContorl(PFocState pFOC,  PSVpwm_State PSVpwm)
 	inv_clarke_transform(pFOC->uAlpha, pFOC->uBeta , &(pFOC->ua), &(pFOC->ub), &(pFOC->uc));
 	
 	//设置PWM
-	MotorSetPwm(pFOC->ua, pFOC->ub, pFOC->uc);
+	//MotorSetPwm(pFOC->ua, pFOC->ub, pFOC->uc);
 	
-	//SVpwm(PSVpwm, pFOC->uAlpha, pFOC->uBeta);
+	SVpwm(PSVpwm, pFOC->uAlpha, pFOC->uBeta);
 	
 	//设置SVPWM
-	//setSVpwm(PSVpwm);
+	setSVpwm(PSVpwm);
 }
 
 
